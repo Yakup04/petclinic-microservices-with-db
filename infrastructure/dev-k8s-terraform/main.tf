de9da2e6380d829bc5257ae2ae1fcb0d2932439a
Yakup04 <yakup041994@gmail.com>
2022-06-25 09:29:20 +0000

    added1 dev-k8s-terraform  for kubernetes10

--- infrastructure/dev-k8s-terraform/main.tf
+++ infrastructure/dev-k8s-terraform/main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
-  region  = "us-east-1"
+  region     = "us-east-1"
+  access_key = "AKIAQBWJCKUS457XN6VU"
+  secret_key = "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"
 }
 
 module "iam" {
